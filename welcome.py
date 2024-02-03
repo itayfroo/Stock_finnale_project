0487ec2be9e763828aee9847b2c4af147e68df2f
itayfroo <151866243+itayfroo@users.noreply.github.com>
2024-02-03 21:42:17 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -14,8 +14,8 @@ def welcome_page():
     st.subheader(translate_word("📚 Available Pages:"))
 
     st.markdown(f"- **{translate_word('User Entrance Field')}**: {translate_word('Navigate to the user entrance field to sign up, sign in, or change user information.')}")
-    st.markdown(f"- **{translate_word('Stock Analysis')}**: Explore the Stock Analysis page to analyze stock prices, make predictions, and check investment opportunities.")
-    st.markdown(f"- **{translate_word('Choose Language')}**: If you prefer a different language, use this page to choose your preferred language.")
+    st.markdown(f"- **{translate_word('Stock Analysis')}**:  {translate_word('Explore the Stock Analysis page to analyze stock prices, make predictions, and check investment opportunities.')}")
+    st.markdown(f"- **{translate_word('Choose Language')}**:  {translate_word('If you prefer a different language, use this page to choose your preferred language.')}")
 
     st.write(translate_word(
         "Feel free to explore the different functionalities and make the most out of the Stock Analyzer App! 📊📈"
