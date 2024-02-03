6fe82d14fb4578cb1e04864dfe36c9e8e00bb70f
itayfroo <151866243+itayfroo@users.noreply.github.com>
2024-02-03 19:52:18 +0200

    Update chooseLangauge.py

--- chooseLangauge.py
+++ chooseLangauge.py
@@ -5,7 +5,23 @@ def language_chooser():
         st.session_state.chosen_language = 'en'
 
     st.header(translate_word("Choose a language"))
-    language_options = [ 'English','Russian','Hebrew']
+    language_options = [
+        'English', 'Russian', 'Hebrew', 'French', 'Spanish', 
+        'German', 'Italian', 'Chinese', 'Japanese', 
+        'Arabic', 'Portuguese', 'Dutch', 'Korean', 'Turkish',
+        'Swedish', 'Greek', 'Polish', 'Thai', 'Hindi',
+        'Czech', 'Finnish', 'Danish', 'Norwegian', 'Indonesian',
+        'Romanian', 'Hungarian', 'Bulgarian', 'Vietnamese', 'Malay',
+        'Slovak', 'Slovenian', 'Croatian', 'Serbian', 'Ukrainian',
+        'Lithuanian', 'Latvian', 'Estonian', 'Albanian', 'Macedonian',
+        'Bosnian', 'Montenegrin', 'Georgian', 'Azerbaijani', 'Kazakh',
+        'Uzbek', 'Tajik', 'Kyrgyz', 'Turkmen', 'Mongolian',
+        'Burmese', 'Khmer', 'Lao', 'Tagalog', 'Malagasy',
+        'Swahili', 'Amharic', 'Tigrinya', 'Somali', 'Hausa',
+        'Yoruba', 'Igbo', 'Zulu', 'Xhosa', 'Sesotho',
+        'Fijian', 'Samoan', 'Tongan', 'Maori', 'Hawaiian',
+        'Marshallese', 'Chamorro', 'Palauan', 'Kiribati', 'Nauruan'
+    ]
     st.session_state.chosen_language = st.selectbox("Choose a language", language_options)
     st.session_state.chosen_language = st.session_state.chosen_language[:2].lower()
 
