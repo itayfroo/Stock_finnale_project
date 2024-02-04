dc26a66af0764baae6666903eabafc5b4f7b2c98
itayfroo <151866243+itayfroo@users.noreply.github.com>
2024-02-04 21:05:05 +0200

    d

--- codes/welcome.py
+++ codes/welcome.py
@@ -5,9 +5,8 @@ def welcome_page():
     st.title(translate_word("🚀 Welcome to Stock Analyzer App!"))
 
     st.image("icons8-stock-48.png", width=200)  
-
+    st.write(translate_word("Explore the world of stocks with our powerful Stock Analyzer App! "))
     st.write(translate_word(
-        "Explore the world of stocks with our powerful Stock Analyzer App! "
         "This app provides you with tools to analyze stock prices, make predictions, and discover investment opportunities. "
         "Navigate through the sidebar options to get started."
     ))
