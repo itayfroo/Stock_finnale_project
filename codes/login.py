06c53aa0e5279dd1a3f43ef939b979835b057bd3
itayfroo <151866243+itayfroo@users.noreply.github.com>
2024-02-19 21:33:57 +0200

    d

--- codes/login.py
+++ codes/login.py
@@ -102,20 +102,23 @@ def sign_in(username, password):
                 stock =st.selectbox(translate_word("Select or enter company name:"), list(company_dict.keys()), index=0).upper()
                 additional_info['City'] = city
                 additional_info['Stock_investment'] = stock
-                if additional_info['Amount_invested'] =="":
-                    amount_invested= st.text_input(translate_word("Enter the amount you want to invest"))
-                    st.button('Confirm', on_click=click_button)
-                    if st.session_state.clicked:
-                        try:
-                            if int(amount_invested) < 0:
-                                st.warning(translate_word("Invalid input"))
-                                amount_invested =""
-                            else: 
-                                additional_info['Amount_invested']=int(amount_invested)
-                                st.balloons()
-                        except:st.warning(translate_word("Invalid input"))
-                else:
-                    additional_info['Amount_invested']=additional_info['Amount_invested']
+                try:
+                    if additional_info['Amount_invested'] =="":
+                        amount_invested= st.text_input(translate_word("Enter the amount you want to invest"))
+                        st.button('Confirm', on_click=click_button)
+                        if st.session_state.clicked:
+                            try:
+                                if int(amount_invested) < 0:
+                                    st.warning(translate_word("Invalid input"))
+                                    amount_invested =""
+                                else: 
+                                    additional_info['Amount_invested']=int(amount_invested)
+                                    st.balloons()
+                            except:st.warning(translate_word("Invalid input"))
+                    else:
+                        additional_info['Amount_invested']=additional_info['Amount_invested']
+                except Exception as e:
+                    st.warning(e)
                 users[username] = user_data
                 users[f"{username}_info"] = {'Age':age,'City':city,'Stock_investment':stock,'Amount_invested':amount_invested}
                 with open(json_file_path, "w") as file:
